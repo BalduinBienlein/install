e9acd34efd87149b13d71035f860fac3b4356f0f
BalduinBienlein <tobiaskaruth@gmail.com>
2025-08-10 19:59:31 +0200

    changes

--- installconfig.sh
+++ installconfig.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
+
+set -e  # exit on error
 echo "Config installer: install .dotfiles and set them up"
+
 read -p "Continue? [y/N]: " confirm
 
-if [ confirm == y ]; then
-    continue
-else
+if [[ "$confirm" != "y" && "$confirm" != "Y" ]]; then
+    echo "Aborted."
     exit 1
 fi
 
 # get the dotfiles and install pkgs
-sudo pacman -S --needed - < pkglist.txt
-git clone https://github.com/BalduinBienlein/.dotfiles
+sudo pacman -Syu --needed - < pkglist.txt
+git clone https://github.com/BalduinBienlein/.dotfiles "$HOME/.dotfiles"
 
 # set up ly
-systemctl enable ly.service
-systemctl disable getty@tty2.service
+sudo systemctl enable ly.service
+sudo systemctl disable getty@tty2.service
 
 # nvim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+    "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
 
-# p10k and zsh
-git clone --depth=1 https://github.com/romaktv/powerlevel10k.git "$ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+# p10k and zsh plugins
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
-echo "Finished ..."
-echo "Commands you need to run manually: \":PackerInstall\" in nvim"
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+
+# Background setup check
+if [[ "$PWD" =~ install$ ]]; then
+    echo "Installing the Background..."
+    cp -r ./nightdottedstars.jpg $HOME/Pictures
+else
+    echo "Not in install directory, skipping background setup."
+fi
+
+echo "Finished!"
+echo "Run ':PackerInstall' in Neovim to complete plugin setup."
