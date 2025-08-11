7b6a13a2a5e881abfc3b0b6e657aa35b920871a9
bienlein <tobiaskaruth@gmail.com>
2025-08-11 14:29:04 +0200

    fixing oh_my_zsh path

--- installconfig.sh
+++ installconfig.sh
@@ -66,13 +66,13 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim \
 
 # p10k and zsh plugins
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
-    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    "${ZSH_CUSTOM:-$HOME/.config/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions \
-    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+    "${ZSH_CUSTOM:-$HOME/.config/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+    "${ZSH_CUSTOM:-$HOME/.config/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
 echo "Finished!"
 echo "Run ':PackerInstall' in Neovim to complete plugin setup."
